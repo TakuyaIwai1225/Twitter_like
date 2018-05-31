b02827bfa5b9188a47141367bbbd3ef10e912682
TakuyaIwai <tha1225.1010@gmail.com>
2018-05-31 03:56:29 +0000

    finish cp8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'staticpages#home'
@@ -6,5 +8,8 @@ Rails.application.routes.draw do
   get  '/about',   to: 'staticpages#about'
   get  '/contact', to: 'staticpages#contact'
   get  '/signup',  to: 'users#new' 
+  get  '/login',   to: 'sessions#new'
+  post  '/login',   to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
